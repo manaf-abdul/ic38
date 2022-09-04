a320aac142158b589f1c801512b6bf6f55c0dabe
manaf-abdul <abdulmanafp1996@gmail.com>
2022-09-04 16:08:59 +0530

    POC of LIVE SHEETS

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import bodyparser from 'body-parser'
+import Axios from 'axios'
 
+dotenv.config()
 const app=express()
 
+app.use(cors());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
-app.listen(5001, console.log(`Server running in 5001`))
+app.get('/',(req,res)=>{
+    res.send("API IS RUNNING !!!!!! :->")
+})
+
+app.get('/get',async(req,res)=>{
+    const {data}=await Axios.get('https://script.googleusercontent.com/macros/echo?user_content_key=6UTQpnRJNRwlU-XnzU3LQFhumGx9UQPStT93MHwmXmxIhRalYGP4Z2c4Rw2HWR1WeAeqMvPhyxt41TCU0Z7DvQjnSM07LOdFm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnH810grv4XdUG_LwF2eyuMa5ufkfaA__S5UK4miFUlt12chAkuCJLDM1zrPx1aiUl95iAjojgKbQq7vfscZ4onCrcFK6sgX0iw&lib=Ms-_GpD7w53h-KAzT1xgMOxu7vu5ZmFTa')
+    // console.log("data",data)
+    res.send(data)
+})
+
+
+
+app.listen(process.env.PORT || 5005, console.log(`Server running in ${process.env.PORT}`))
